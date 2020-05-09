717495a0a6635a66c71c70c632b7b32a5d778656
arpan sahu <arpanrocks95@gmail.com>
2020-05-09 20:52:51 +0530

    Create Quick_Sort_Recusrive.cpp

---
+++ Quick_Sort_Recusrive.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int Partition(int a[], int start, int end)
+{
+    int pivot = a[end];
+    int pIndex = start;
+
+    for (int i = start; i < end; i++)
+    {
+        if (a[i] <= pivot)
+        {
+            swap(a[i], a[pIndex]);
+            pIndex++;
+        }
+    }
+    swap (a[pIndex], a[end]);
+    return pIndex;
+}
+void QuickSort(int arr[],int start,int end){
+    if(start >=  end ) return;
+    int pivot  = Partition(arr,start,end);
+    QuickSort(arr,start,pivot -  1);
+    QuickSort(arr,pivot,end);
+}
+int main()
+{
+    int arr[] = { 3, 8, 5, 4, 1, 9, -2 };
+    int size = (*(&arr + 1) - arr);
+
+    QuickSort(arr, 0,size - 1);
+    
+    for (int k = 0; k < (*(&arr + 1) - arr); ++k) {
+        cout<<arr[k]<<" ";
+    }cout<<"\n";
+
+    return 0;
+}
